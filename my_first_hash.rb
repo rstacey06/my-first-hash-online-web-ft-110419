b814b7c65cdf6479912c6a4e6e4630ebe5193a1b
Burbage Stacey <rstacey06@gmail.com>
2019-11-08 15:43:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-shipping_manifest["oil paintings"]
+shipping_manifest["oil painting"]
 
 end
 
@@ -24,7 +24,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest[mus]
+
 
 
   # add 4 gun powder to the shipping_manifest hash below
