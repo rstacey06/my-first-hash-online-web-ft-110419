8b2a2da1eebd69dc389abdd40abf2495db452807
Burbage Stacey <rstacey06@gmail.com>
2019-11-08 15:26:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  me= {"name" => "Ray", "occupation" => "student", "hobbies" => "travel"}
 end
 
 
